69cc7a0abcbebcd32f1252787eea21f681d1f51e
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-05-27 13:13:06 -0400

    made port variable lower case

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require('./app.js');
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+app.listen(port, () => console.log(`Listening on port: ${port}`))
